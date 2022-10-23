ef28c8949a316abb1740349150e2a6d5e7b9d4e4
Utkarsha <utkarsha@Utkarshas-MacBook-Air.local>
2022-10-23 01:41:26 -0400

    multireporting

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "rfzn6m",
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -9,5 +10,9 @@ module.exports = defineConfig({
     },
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
+    },
   },
 });
